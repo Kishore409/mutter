82e1a9fd91ffe350c4791ddafc09439812d33b77
Jonas Ådahl <jadahl@gmail.com>
2018-07-26 11:28:01 +0200

    wayland/gtk-shell: Add timestamp-less passive_present request
    For when Gtk+ wants to present a surface but doesn't have a valid
    timestamp (e.g. a non-interacted-with window got a new activate request
    e.g. new file was opened), we don't to have Gtk+ have to fake a
    timestamp to be able to ask to be presented. Thus, enable it to request
    to be activated but without providing a timestamp. Currently this is
    implemented by always calling "needs attention" on the corresponding
    window.

--- src/wayland/meta-wayland-gtk-shell.c
+++ src/wayland/meta-wayland-gtk-shell.c
@@ -132,11 +132,26 @@ gtk_surface_present (struct wl_client   *client,
                              META_CLIENT_TYPE_APPLICATION, NULL);
 }
 
+static void
+gtk_surface_passive_present (struct wl_client   *client,
+                             struct wl_resource *resource)
+{
+  MetaWaylandGtkSurface *gtk_surface = wl_resource_get_user_data (resource);
+  MetaWaylandSurface *surface = gtk_surface->surface;
+  MetaWindow *window = surface->window;
+
+  if (!window)
+    return;
+
+  meta_window_set_demands_attention (window);
+}
+
 static const struct gtk_surface1_interface meta_wayland_gtk_surface_interface = {
   gtk_surface_set_dbus_properties,
   gtk_surface_set_modal,
   gtk_surface_unset_modal,
   gtk_surface_present,
+  gtk_surface_passive_present,
 };
 
 static void
