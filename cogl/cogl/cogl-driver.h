02fd26edb831cdd73dbc2c2297961293f37b649f
Jonas Ådahl <jadahl@gmail.com>
2020-10-19 21:19:15 +0200

    cogl/gl-framebuffer: Split up into FBO and back drivers
    One is for when we're painting to the back buffer (onscreen), and the
    other when we're painting to an FBO (offscreen).

--- cogl/cogl/cogl-driver.h
+++ cogl/cogl/cogl-driver.h
@@ -95,10 +95,6 @@ struct _CoglDriverVtable
                          float alpha);
 
   void
-  (* framebuffer_query_bits) (CoglFramebuffer *framebuffer,
-                              CoglFramebufferBits *bits);
-
-  void
   (* framebuffer_finish) (CoglFramebuffer *framebuffer);
 
   void
